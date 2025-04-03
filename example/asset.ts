7e1eec75a59adae08bc00406c501456e06d8fb59
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-03 13:28:59 +0200

    add working example script

--- example/asset.ts
+++ example/asset.ts
@@ -1,16 +1,16 @@
-import { decode } from "https://deno.land/std@0.97.0/encoding/base64.ts";
+import {decode} from 'https://deno.land/std@0.97.0/encoding/base64.ts';
 
 const bundledObject = {
-  files:{
-    "test-text":{
-      content:decode("SGVsbG8gV29ybGQhIQ=="),
-      extension: "txt"
-    },
-    "test-text2":{
-      content:decode("SGVsbG8gV29ybGQhIQ=="),
-      extension: "txt"
-    }
-  }
-} 
+	files: {
+		'test-text': {
+			content: decode('SGVsbG8gV29ybGQhIQ=='),
+			extension: 'txt',
+		},
+		'test-text2': {
+			content: decode('SGVsbG8gV29ybGQhIQ=='),
+			extension: 'txt',
+		},
+	},
+};
 
-export default bundledObject
+export default bundledObject;
